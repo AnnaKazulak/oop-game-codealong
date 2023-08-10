9d496081a9542bf1ec6d8bdbefab9180087f70ce
Anna Kazulak <anna.kazulak@gmail.com>
2023-08-10 16:35:15 +0200

    add obstacles

--- js/main.js
+++ js/main.js
@@ -34,7 +34,51 @@ class Player {
   }
 }
 
+class Obstacle {
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 100;
+    this.width = 20;
+    this.height = 10;
+    this.domElement = null;
+    this.createDomElement();
+  }
+
+  createDomElement() {
+    // create dom element
+    this.domElement = document.createElement("div");
+
+    // set id
+    this.domElement.className = "obstacle";
+    this.domElement.style.width = this.width + "vw";
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.bottom = this.positionY + "vh";
+
+    //append to the dom
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.domElement);
+  }
+  moveDown() {
+    console.log("move down");
+    this.positionY -= 2;
+    this.domElement.style.bottom = this.positionY + "vh";
+  }
+}
 const player = new Player();
+
+const obstacleArray = []; //will store instances of the class Obstacle
+
+setInterval(() => {
+  const newObstacle = new Obstacle();
+  obstacleArray.push(newObstacle);
+}, 3000);
+
+setInterval(() => {
+  obstacleArray.forEach((obstacleInstance) => {
+    obstacleInstance.moveDown();
+  });
+}, 100);
 
 document.addEventListener("keydown", (event) => {
   if (event.code == "ArrowLeft") {
